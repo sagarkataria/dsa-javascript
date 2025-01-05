e73ae0ea832c5f3242e8e6c147780186ff493ae6
sagarkataria <sagarsingh0110@gmail.com>
2025-01-05 18:45:44 +0530

    binary serach -> search elements

---
+++ BinarySearch/Problem-1.js
@@ -0,0 +1,20 @@
+const arr = [2, 4, 7, 11, 1, 19, 21];
+
+function searchKey(arr, key) {
+    let start = 0;
+    let end = arr.length;
+    
+    while (start <= end) {
+        let m = Math.floor((start + end) / 2);
+        if (arr[m] === key) {
+            return m;
+        }
+        else if (arr[m] < key) {
+            start = m + 1;
+        } else {
+            end = m - 1;
+        }
+    }
+}
+
+console.log(searchKey(arr, 4));
