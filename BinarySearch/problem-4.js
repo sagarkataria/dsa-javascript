4dd75852fa2563261cb9743274e3d63e0ac5e4f7
sagarkataria <sagarsingh0110@gmail.com>
2025-01-07 21:36:08 +0530

    for every cases

--- BinarySearch/problem-4.js
+++ BinarySearch/problem-4.js
@@ -2,15 +2,18 @@
 
 function sqrt(x) {
 
+    if(x<2){
+        return x;
+    }
     let start = 0, end = x;
     let ans, mid;
 
     while (start <= end) {
         mid = Math.floor(start + (end - start) / 2);
-        if (mid * mid === x) {
+        if (mid  === x/mid) {
             ans = mid;
             break;
-        } else if (mid * mid < x) {
+        } else if (mid < x/mid) {
             start = mid + 1;
             ans = mid;
         } else {
